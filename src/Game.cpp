0e012c6eb2d997aecf1f162ca3ff734bb7ab8e2f
Tartiine <sofia.saadi6@gmail.com>
2024-06-26 12:57:40 +0200

    Refactor : delete useless this

--- src/Game.cpp
+++ src/Game.cpp
@@ -13,20 +13,20 @@ using namespace std;
 
 Game::Game() : showGamepadFlag(true) {
     loadFonts();
-    this->initWindow();
-    this->initRenderTexture();
-    this->initMap();
-    this->initPlayer();
-    this->initStartScreen();
+    initWindow();
+    initRenderTexture();
+    initMap();
+    initPlayer();
+    initStartScreen();
     checkGamepad();
 }
 
 Game::~Game() {
-    this->obstacles.clear();
+    obstacles.clear();
 }
 
 void Game::collisionPlayer() {
-    this->player->checkWindowBorders(this->renderTexture);
+    player->checkWindowBorders(renderTexture);
 
     auto renderedTileMaps = tileMapManager->getRenderedTileMaps();
 
@@ -36,7 +36,7 @@ void Game::collisionPlayer() {
                 auto& obstacle = tileMap->getTile(i, j);
                 if (obstacle) {
                     sf::FloatRect bounds = obstacle->getHitbox();
-                    if (this->player->isColliding(bounds)) {
+                    if (player->isColliding(bounds)) {
                         player->resolveCollision(bounds);
                     }
                 }
@@ -46,93 +46,93 @@ void Game::collisionPlayer() {
 }
 
 void Game::updatePlayer(float deltaTime) {
-    this->player->update(deltaTime);
+    player->update(deltaTime);
 }
 
 void Game::update(float deltaTime) {
     sf::Event event;
-    while (this->window.pollEvent(event)) {
+    while (window.pollEvent(event)) {
         if (event.type == sf::Event::Closed) {
-            this->window.close();
+            window.close();
         } else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-            this->window.close();
+            window.close();
         } else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Enter) {
             if (!gameStarted) {
                 gameStarted = true;
             }
         }
         else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::F) {
-            this->window.close();
-            if (this->isFullscreenOn) {
-                this->scale = 2;
+            window.close();
+            if (isFullscreenOn) {
+                scale = 2;
                 window.setMouseCursorVisible(true);
-                this->initWindow();
+                initWindow();
             } else {
                 window.setMouseCursorVisible(false);
-                this->initWindowFullscreen();  
+                initWindowFullscreen();  
             }
         } else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::G) {
             scale = (scale % 3) + 1;
-            this->window.close();
-            this->initWindow();
+            window.close();
+            initWindow();
         }
     }
 
     if (gameStarted) {
-        this->updatePlayer(deltaTime);
-        this->collisionPlayer();
-        this->tileMapManager->update(deltaTime, this->player.get(), this->renderTexture);
+        updatePlayer(deltaTime);
+        collisionPlayer();
+        tileMapManager->update(deltaTime, player.get(), renderTexture);
         tileMapManager->updateAnimation(deltaTime);
-        if (tileMapManager->checkPortal(this->player.get())) {
+        if (tileMapManager->checkPortal(player.get())) {
             showEndMenu();
         }
     }
 }
 
 void Game::renderPlayer(){
-    this->player->render(this->renderTexture);
+    player->render(renderTexture);
 }
 
 void Game::renderObstacles(bool debug) {
-    tileMapManager->render(this->renderTexture, debug);
+    tileMapManager->render(renderTexture, debug);
 }
 
 void Game::render() {
     if (!gameStarted) {
-        this->initStartScreen();
-        this->window.clear();
-        this->window.draw(startBackground);
-        this->window.draw(startText);
-        this->window.draw(secondaryText);
-        this->window.display();
+        initStartScreen();
+        window.clear();
+        window.draw(startBackground);
+        window.draw(startText);
+        window.draw(secondaryText);
+        window.display();
         return;
     }
 
-    this->renderTexture.clear(sf::Color(0, 120, 226));
+    renderTexture.clear(sf::Color(0, 120, 226));
 
-    this->renderObstacles(false);
-    this->renderPlayer();
+    renderObstacles(false);
+    renderPlayer();
 
     renderTexture.display();
 
     sf::Sprite renderSprite(renderTexture.getTexture());
-    renderSprite.setScale(this->scale, this->scale);
-    renderSprite.setPosition(this->fullscreenHorizontalOffset, this->fullscreenVerticalOffset);
+    renderSprite.setScale(scale, scale);
+    renderSprite.setPosition(fullscreenHorizontalOffset, fullscreenVerticalOffset);
     
-    this->window.clear();
-    this->window.draw(renderSprite);
+    window.clear();
+    window.draw(renderSprite);
 
     if (showGamepadFlag && infoClock.getElapsedTime().asSeconds() < 3) {
-        this->window.draw(gamepadText);
+        window.draw(gamepadText);
     } else {
         showGamepadFlag = false;
     }
 
-    this->window.display();  
+    window.display();  
 }
 
 const sf::RenderWindow& Game::getWindow() const {
-    return this->window;
+    return window;
 }
 
 void Game::checkGamepad() {
@@ -147,15 +147,15 @@ void Game::checkGamepad() {
 }
 
 void Game::trainModel() {
-    this->tileMapModel = make_unique<TileMapModel>(40*22, 1);
-    this->tileMapModel->createModel("rb");
-    this->tileMapModel->train("resources/rb_maps");
-    this->tileMapModel->saveModel("resources/trained_model_rb.net");
+    tileMapModel = make_unique<TileMapModel>(40*22, 1);
+    tileMapModel->createModel("rb");
+    tileMapModel->train("resources/rb_maps");
+    tileMapModel->saveModel("resources/trained_model_rb.net");
 
-    this->tileMapModel = make_unique<TileMapModel>(40*22, 1);
-    this->tileMapModel->createModel("nb");
-    this->tileMapModel->train("resources/nb_maps");
-    this->tileMapModel->saveModel("resources/trained_model_nb.net");
+    tileMapModel = make_unique<TileMapModel>(40*22, 1);
+    tileMapModel->createModel("nb");
+    tileMapModel->train("resources/nb_maps");
+    tileMapModel->saveModel("resources/trained_model_nb.net");
 }
 
 
@@ -173,30 +173,30 @@ void Game::createGamepadText(bool gamepadConnected) {
 }
 
 void Game::initWindow() {
-    this->window.create(sf::VideoMode(this->resolution.x * this->scale, this->resolution.y * this->scale), "Maze Paradox", sf::Style::Close | sf::Style::Titlebar);
-    this->window.setFramerateLimit(240);
-    this->isFullscreenOn = false;
-    this->fullscreenHorizontalOffset = 0;
-    this->fullscreenVerticalOffset = 0;
+    window.create(sf::VideoMode(resolution.x * scale, resolution.y * scale), "Maze Paradox", sf::Style::Close | sf::Style::Titlebar);
+    window.setFramerateLimit(240);
+    isFullscreenOn = false;
+    fullscreenHorizontalOffset = 0;
+    fullscreenVerticalOffset = 0;
 }
 
 void Game::initWindowFullscreen() {
     sf::VideoMode fullScreen = sf::VideoMode::getFullscreenModes()[0];
 
-    this->window.create(fullScreen, "SFML Platformer", sf::Style::Fullscreen);
-    this->window.setFramerateLimit(240);
-    this->isFullscreenOn = true;
-    this->scale = min(fullScreen.width / this->resolution.x, fullScreen.height / this->resolution.y);
-    this->fullscreenHorizontalOffset = (fullScreen.width - (this->resolution.x * this->scale)) / 2;
-    this->fullscreenVerticalOffset = (fullScreen.height - (this->resolution.y * this->scale)) / 2;
+    window.create(fullScreen, "SFML Platformer", sf::Style::Fullscreen);
+    window.setFramerateLimit(240);
+    isFullscreenOn = true;
+    scale = min(fullScreen.width / resolution.x, fullScreen.height / resolution.y);
+    fullscreenHorizontalOffset = (fullScreen.width - (resolution.x * scale)) / 2;
+    fullscreenVerticalOffset = (fullScreen.height - (resolution.y * scale)) / 2;
 }
 
 void Game::initRenderTexture() {
-    this->renderTexture.create(this->resolution.x, this->resolution.y);
+    renderTexture.create(resolution.x, resolution.y);
 }
 
 void Game::initPlayer() {
-    this->player = make_unique<Player>(64, 300);
+    player = make_unique<Player>(64, 300);
 }
 
 void Game::initMap() {
@@ -208,14 +208,14 @@ void Game::initMap() {
     rbGenerator->generateBatch(25, 40, 22, "resources/maps1/generated_map");
 
     
-    this->tileMapModel = make_unique<TileMapModel>(40 * 22, 1);
-    this->tileMapModel->testModel("resources/maps", "resources/trained_model_nb.net");
-    this->tileMapModel->testModel("resources/maps1", "resources/trained_model_rb.net");
+    tileMapModel = make_unique<TileMapModel>(40 * 22, 1);
+    tileMapModel->testModel("resources/maps", "resources/trained_model_nb.net");
+    tileMapModel->testModel("resources/maps1", "resources/trained_model_rb.net");
     */
     
     tileMapManager = make_unique<TileMapManager>();
     vector<string> directories = {"resources/maps", "resources/maps1"};
-    tileMapManager->generateTileMapOrder(directories, "resources/tile_map_order.txt", this->resolution.x, this->resolution.y);
+    tileMapManager->generateTileMapOrder(directories, "resources/tile_map_order.txt", resolution.x, resolution.y);
     tileMapManager->loadTileMaps("resources/tile_map_order.txt");
     tileMapManager->createFinalMap();
 
@@ -224,13 +224,13 @@ void Game::initMap() {
 void Game::loadFonts(){
     if (!startFont.loadFromFile("resources/fonts/SuperSkinnyPixelBricks-3xKL.ttf")) {
         cerr << "Error: Could not load font 'resources/fonts/SuperSkinnyPixelBricks-3xKL.ttf'" << endl;
-        this->window.close();
+        window.close();
         return;
     }
 
     if (!secondaryFont.loadFromFile("resources/fonts/computer-says-no.ttf")) {
         cerr << "Error: Could not load font 'resources/fonts/computer-says-no.ttf'" << endl;
-        this->window.close();
+        window.close();
         return;
     }
 }
@@ -246,7 +246,7 @@ void Game::initStartScreen()
     sf::FloatRect textRect = startText.getLocalBounds();
     startText.setOrigin(textRect.left + textRect.width / 2.0f,
                         textRect.top + textRect.height / 2.0f);
-    startText.setPosition(this->window.getSize().x / 2.0f - 20, this->window.getSize().y / 2.0f);
+    startText.setPosition(window.getSize().x / 2.0f - 20, window.getSize().y / 2.0f);
 
     secondaryText.setFont(secondaryFont);
     secondaryText.setString("Press Enter to Start");
@@ -257,10 +257,10 @@ void Game::initStartScreen()
     sf::FloatRect secondaryTextRect = secondaryText.getLocalBounds();
     secondaryText.setOrigin(secondaryTextRect.left + secondaryTextRect.width / 2.0f,
                             secondaryTextRect.top + secondaryTextRect.height / 2.0f);
-    secondaryText.setPosition(this->window.getSize().x / 2.0f,
+    secondaryText.setPosition(window.getSize().x / 2.0f,
                               startText.getPosition().y + textRect.height / 2.0f + secondaryTextRect.height / 2.0f + 20.f);
 
-    startBackground.setSize(sf::Vector2f(this->window.getSize().x, this->window.getSize().y)); 
+    startBackground.setSize(sf::Vector2f(window.getSize().x, window.getSize().y)); 
     startBackground.setFillColor(sf::Color::Black);
 }
 
@@ -273,27 +273,27 @@ void Game::showEndMenu() {
 
     sf::FloatRect quitTextRect = quitText.getLocalBounds();
     quitText.setOrigin(quitTextRect.left + quitTextRect.width / 2.0f, quitTextRect.top + quitTextRect.height / 2.0f);
-    quitText.setPosition(this->window.getSize().x / 2.0f, this->window.getSize().y / 2.0f - 50);
+    quitText.setPosition(window.getSize().x / 2.0f, window.getSize().y / 2.0f - 50);
 
     sf::FloatRect replayTextRect = replayText.getLocalBounds();
     replayText.setOrigin(replayTextRect.left + replayTextRect.width / 2.0f, replayTextRect.top + replayTextRect.height / 2.0f);
-    replayText.setPosition(this->window.getSize().x / 2.0f, this->window.getSize().y / 2.0f + 50);
+    replayText.setPosition(window.getSize().x / 2.0f, window.getSize().y / 2.0f + 50);
 
-    this->window.clear();
-    this->window.draw(quitText);
-    this->window.draw(replayText);
-    this->window.display();
+    window.clear();
+    window.draw(quitText);
+    window.draw(replayText);
+    window.display();
 
     bool choiceMade = false;
     while (!choiceMade) {
         sf::Event event;
-        while (this->window.pollEvent(event)) {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::KeyPressed) {
                 if (event.key.code == sf::Keyboard::Q) {
-                    this->window.close();
+                    window.close();
                     choiceMade = true;
                 } else if (event.key.code == sf::Keyboard::R) {
-                    this->resetGame(); 
+                    resetGame(); 
                     choiceMade = true;
                 }
             }
@@ -302,8 +302,8 @@ void Game::showEndMenu() {
 }
 
 void Game::resetGame() {
-    this->initPlayer();
-    this->initMap();
+    initPlayer();
+    initMap();
     gameStarted = false;
 }
 
